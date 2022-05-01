f888f25d3f317f8584948e79d7963a366f526e5b
deowansajal <deowansajal@gmail.com>
2022-05-02 05:36:57 +0600

    add production public_base_url

--- client-02/src/utils/constants.js
+++ client-02/src/utils/constants.js
@@ -1,5 +1,5 @@
 export const PUBLIC_IMAGES_BASE_URL =
-    'http://localhost:4000/public/uploads/images/'
+    'https://f1-blog.herokuapp.com/public/uploads/images/'
 
 export const DELETE_USER = 'DELETE_USER'
 export const UPDATE_USER_ROLE = 'UPDATE_USER_ROLE'
