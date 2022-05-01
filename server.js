5dab5cab2ccc08b1374af2589e254d7d1256c52f
deowansajal <deowansajal@gmail.com>
2022-05-02 03:47:39 +0600

    Add config directory

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 
 const connectDB = require('./config/db')
+
 const { errorHandler } = require('./middleware/errorHandler')
 const routes = require('./routes')
 
