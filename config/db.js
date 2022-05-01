36d5f40e2dffdcc7e4fc071d6d1fdd25593d1b0b
deowansajal <deowansajal@gmail.com>
2022-05-02 03:58:31 +0600

    add config/db.js file

--- config/db.js
+++ config/db.js
@@ -4,8 +4,8 @@ const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
-            // useCreateIndex: true,
-            // useFindAndModify: false,
+            useCreateIndex: true,
+            useFindAndModify: false,
             useUnifiedTopology: true,
         })
         console.log(`MongoDB Connected: ${conn.connection.host}`)
