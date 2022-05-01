e1f9058249956ff8143fb20a3aa65723b2b0bca3
deowansajal <deowansajal@gmail.com>
2022-05-02 04:28:41 +0600

    add MONGO_URI enviroment variables

--- config/db.js
+++ config/db.js
@@ -4,8 +4,7 @@ const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
-            useCreateIndex: true,
-            useFindAndModify: false,
+
             useUnifiedTopology: true,
         })
         console.log(`MongoDB Connected: ${conn.connection.host}`)
